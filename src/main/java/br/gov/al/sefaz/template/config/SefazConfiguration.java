3f0f4e4df99f98f0551cc3fa2e7c6639e8577640
Antonio R. de Oliveira <antonio.a.oliveira@capgemini.com>
2023-09-15 08:18:57 -0300

    Integração com lib sfz-database-spring
    * Configurações de banco de dados comuns providas pela biblioteca
    * Configuração de histórico de banco de dados pela lib
    
    sefaz-al/ti/arquitetura/arquitetura-info#1051

--- src/main/java/br/gov/al/sefaz/template/config/SefazConfiguration.java
+++ src/main/java/br/gov/al/sefaz/template/config/SefazConfiguration.java
@@ -1,9 +1,11 @@
 package br.gov.al.sefaz.template.config;
 
+import br.gov.al.sefaz.database.config.EnableSefazOracleDatabase;
 import br.gov.al.sefaz.logging.spring.EnableSefazLogging;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSefazLogging
+@EnableSefazOracleDatabase
 public class SefazConfiguration {
 }
