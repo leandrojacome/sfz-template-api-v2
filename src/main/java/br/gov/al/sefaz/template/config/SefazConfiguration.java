844c8a91ad01f4f8507a263b6596cb3b94563ffe
Antonio R. de Oliveira <antonio.a.oliveira@capgemini.com>
2023-09-18 16:45:56 -0300

    Registro do bean UsuarioAnonimoFornecedoridentidadeHistoricoDados no template.
    sefaz-al/ti/arquitetura/arquitetura-info#1051

--- src/main/java/br/gov/al/sefaz/template/config/SefazConfiguration.java
+++ src/main/java/br/gov/al/sefaz/template/config/SefazConfiguration.java
@@ -1,11 +1,19 @@
 package br.gov.al.sefaz.template.config;
 
 import br.gov.al.sefaz.database.config.EnableSefazOracleDatabase;
+import br.gov.al.sefaz.database.historico.UsuarioAnonimoFornecedoridentidadeHistoricoDados;
 import br.gov.al.sefaz.logging.spring.EnableSefazLogging;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSefazLogging
 @EnableSefazOracleDatabase
 public class SefazConfiguration {
+
+    @Bean
+    UsuarioAnonimoFornecedoridentidadeHistoricoDados fornecedoridentidadeHistoricoDados() {
+        return new UsuarioAnonimoFornecedoridentidadeHistoricoDados();
+    }
+
 }
